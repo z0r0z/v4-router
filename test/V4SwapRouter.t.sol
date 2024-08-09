779a8b2993340f52f002b2d88b27d991b1468c66
ross <92001561+z0r0z@users.noreply.github.com>
2024-08-09 12:18:59 +0000

    🥢

--- test/V4SwapRouter.t.sol
+++ test/V4SwapRouter.t.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.19;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
 
 import {Key, Swap, V4SwapRouter} from "../src/V4SwapRouter.sol";
 import {IPoolManager, PoolManager} from "@v4/src/PoolManager.sol";

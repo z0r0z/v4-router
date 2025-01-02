695615699da2b9a893f5da9a18e6cfde64a5b8be
saucepoint <saucepoint@protonmail.com>
2025-01-02 16:39:55 -0500

    layout multihop setup

--- test/V4SwapRouter.multihop.t.sol
+++ test/V4SwapRouter.multihop.t.sol
@@ -1,327 +1,68 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-import {GasSnapshot} from "forge-gas-snapshot/GasSnapshot.sol";
+import {Hooks} from "@v4/src/libraries/Hooks.sol";
+import {PoolKey} from "@v4/src/types/PoolKey.sol";
+import {Currency} from "@v4/src/types/Currency.sol";
 
 import {V4SwapRouter} from "../src/V4SwapRouter.sol";
-import {IPoolManager, PoolManager} from "@v4/src/PoolManager.sol";
 
-import {PoolKey} from "@v4/src/types/PoolKey.sol";
-import {IHooks} from "@v4/src/interfaces/IHooks.sol";
-import {Currency, CurrencyLibrary} from "@v4/src/types/Currency.sol";
+import {SwapRouterFixtures, Deployers} from "./utils/SwapRouterFixtures.sol";
+import {MockCurrencyLibrary} from "./utils/mocks/MockCurrencyLibrary.sol";
 
-import {Test} from "../lib/forge-std/src/Test.sol";
-import {MockERC20} from "@solady/test/utils/mocks/MockERC20.sol";
+contract MultihopTest is SwapRouterFixtures {
+    using MockCurrencyLibrary for Currency;
 
-import {NoOpSwapHook} from "./utils/mocks/hooks/NoOpSwapHook.sol";
+    V4SwapRouter router;
 
-import {PoolModifyLiquidityTest} from "@v4/src/test/PoolModifyLiquidityTest.sol";
-
-import {PathKey} from "../src/libraries/PathKey.sol";
-
-contract V4SwapRouterTest is Test, GasSnapshot {
-    address internal aliceSwapper;
-
-    address internal manager;
-    V4SwapRouter internal router;
-
-    PoolModifyLiquidityTest internal liqRouter;
-
-    address internal currency0Addr;
-    address internal currency1Addr;
-    address internal currency2Addr;
-    address internal currency3Addr;
-
-    // Min tick for full range with tick spacing of 60.
-    int24 internal constant MIN_TICK = -887220;
-    // Max tick for full range with tick spacing of 60.
-    int24 internal constant MAX_TICK = -MIN_TICK;
-
-    // Vanilla pool (no hook).
-    PoolKey internal keyNoHook;
-
-    // Vanilla variant for cf.
-    PoolKey internal keyNoHook2;
-
-    // Vanilla variant for cf.
-    PoolKey internal keyNoHook3;
-
-    // Vanilla variant for cf.
-    PoolKey internal keyNoHook4;
-
-    // Vanilla variant for cf.
-    PoolKey internal keyNoHook5;
-
-    // ETH based pool (no hook).
-    PoolKey internal ethKeyNoHook;
-
-    // Basic no-op hook pool.
-    PoolKey internal keyNoOpSwapHook;
-
-    // Basic no-op hook for testing.
-    IHooks internal noOpSwapHook;
-
-    // floor(sqrt(1) * 2^96)
-    uint160 constant startingPrice = 79228162514264337593543950336;
+    PoolKey[] vanillaPoolKeys;
+    PoolKey[] nativePoolKeys;
+    PoolKey[] hookedPoolKeys;
+    PoolKey[] csmmPoolKeys;
 
     function setUp() public payable {
-        aliceSwapper = makeAddr("alice");
-        payable(aliceSwapper).transfer(1 ether);
+        // Deploy v4 contracts
+        Deployers.deployFreshManagerAndRouters();
+        router = new V4SwapRouter(manager);
 
-        manager = address(new PoolManager(address(this)));
-        router = new V4SwapRouter(IPoolManager(manager));
+        // Create currencies
+        (currencyA, currencyB, currencyC, currencyD) = _createSortedCurrencies();
 
-        liqRouter = new PoolModifyLiquidityTest(IPoolManager(manager));
+        currencyA.mint(address(this), 10_000e18);
+        currencyB.mint(address(this), 10_000e18);
+        currencyC.mint(address(this), 10_000e18);
+        currencyD.mint(address(this), 10_000e18);
 
-        address[] memory addrs = new address[](4);
+        currencyA.maxApprove(address(modifyLiquidityRouter));
+        currencyB.maxApprove(address(modifyLiquidityRouter));
+        currencyC.maxApprove(address(modifyLiquidityRouter));
+        currencyD.maxApprove(address(modifyLiquidityRouter));
 
-        addrs[0] = address(new MockERC20("Test0", "Test0", 18));
-        addrs[1] = address(new MockERC20("Test1", "Test2", 18));
-        addrs[2] = address(new MockERC20("Test2", "Test2", 18));
-        addrs[3] = address(new MockERC20("Test3", "Test3", 18));
+        // TODO: deploy hooks
 
-        addrs = _sortAddresses(addrs);
-        currency0Addr = addrs[0];
-        currency1Addr = addrs[1];
-        currency2Addr = addrs[2];
-        currency3Addr = addrs[3];
+        // Define and create pools with liquidity
+        PoolKey[] memory _vanillaPoolKeys = _createPoolKeys(address(0));
+        _copyArrayToStorage(_vanillaPoolKeys, vanillaPoolKeys);
+        PoolKey[] memory _nativePoolKeys = _createNativePoolKeys(address(0));
+        _copyArrayToStorage(_nativePoolKeys, nativePoolKeys);
+        PoolKey[] memory _hookedPoolKeys = _createPoolKeys(address(Hooks.BEFORE_SWAP_FLAG));
+        _copyArrayToStorage(_hookedPoolKeys, hookedPoolKeys);
+        PoolKey[] memory _csmmPoolKeys = _createPoolKeys(address(Hooks.AFTER_SWAP_FLAG));
+        _copyArrayToStorage(_csmmPoolKeys, csmmPoolKeys);
 
-        MockERC20(currency0Addr).mint(aliceSwapper, 100 ether);
-        MockERC20(currency1Addr).mint(aliceSwapper, 100 ether);
-        MockERC20(currency2Addr).mint(aliceSwapper, 100 ether);
-        MockERC20(currency3Addr).mint(aliceSwapper, 100 ether);
-
-        vm.prank(aliceSwapper);
-        MockERC20(currency0Addr).approve(address(router), type(uint256).max);
-        vm.prank(aliceSwapper);
-        MockERC20(currency1Addr).approve(address(router), type(uint256).max);
-        vm.prank(aliceSwapper);
-        MockERC20(currency2Addr).approve(address(router), type(uint256).max);
-        vm.prank(aliceSwapper);
-        MockERC20(currency3Addr).approve(address(router), type(uint256).max);
-
-        vm.prank(aliceSwapper);
-        MockERC20(currency0Addr).approve(address(liqRouter), type(uint256).max);
-        vm.prank(aliceSwapper);
-        MockERC20(currency1Addr).approve(address(liqRouter), type(uint256).max);
-        vm.prank(aliceSwapper);
-        MockERC20(currency2Addr).approve(address(liqRouter), type(uint256).max);
-        vm.prank(aliceSwapper);
-        MockERC20(currency3Addr).approve(address(liqRouter), type(uint256).max);
-
-        keyNoHook = PoolKey({
-            currency0: Currency.wrap(currency0Addr),
-            currency1: Currency.wrap(currency1Addr),
-            fee: 3000,
-            tickSpacing: 60,
-            hooks: IHooks(address(0))
-        });
-
-        PoolManager(manager).initialize(keyNoHook, startingPrice);
-
-        keyNoHook2 = PoolKey({
-            currency0: Currency.wrap(currency2Addr),
-            currency1: Currency.wrap(currency3Addr),
-            fee: 3000,
-            tickSpacing: 60,
-            hooks: IHooks(address(0))
-        });
-
-        PoolManager(manager).initialize(keyNoHook2, startingPrice);
-
-        keyNoHook3 = PoolKey({
-            currency0: Currency.wrap(currency1Addr),
-            currency1: Currency.wrap(currency3Addr),
-            fee: 3000,
-            tickSpacing: 60,
-            hooks: IHooks(address(0))
-        });
-
-        PoolManager(manager).initialize(keyNoHook3, startingPrice);
-
-        keyNoHook4 = PoolKey({
-            currency0: Currency.wrap(currency1Addr),
-            currency1: Currency.wrap(currency2Addr),
-            fee: 3000,
-            tickSpacing: 60,
-            hooks: IHooks(address(0))
-        });
-
-        PoolManager(manager).initialize(keyNoHook4, startingPrice);
-
-        keyNoHook5 = PoolKey({
-            currency0: Currency.wrap(currency0Addr),
-            currency1: Currency.wrap(currency3Addr),
-            fee: 3000,
-            tickSpacing: 60,
-            hooks: IHooks(address(0))
-        });
-
-        PoolManager(manager).initialize(keyNoHook5, startingPrice);
-
-        /*noOpSwapHook = IHooks(address(new NoOpSwapHook(IPoolManager(manager))));
-
-        keyNoOpSwapHook = PoolKey({
-            currency0: Currency.wrap(currency0Addr),
-            currency1: Currency.wrap(currency1Addr),
-            fee: 3000,
-            tickSpacing: 60,
-            hooks: noOpSwapHook
-        });
-
-        PoolManager(manager).initialize(keyNoOpSwapHook, startingPrice);*/
-
-        ethKeyNoHook = PoolKey({
-            currency0: Currency.wrap(address(0)),
-            currency1: Currency.wrap(currency1Addr),
-            fee: 3000,
-            tickSpacing: 60,
-            hooks: IHooks(address(0))
-        });
-
-        PoolManager(manager).initialize(ethKeyNoHook, startingPrice);
-
-        int24 tickLower = -600;
-        int24 tickUpper = 600;
-        int256 liquidity = 20 ether;
-
-        payable(aliceSwapper).transfer(uint256(liquidity));
-
-        vm.prank(aliceSwapper);
-        liqRouter.modifyLiquidity(
-            keyNoHook,
-            IPoolManager.ModifyLiquidityParams({
-                tickLower: tickLower,
-                tickUpper: tickUpper,
-                liquidityDelta: liquidity,
-                salt: 0
-            }),
-            ""
-        );
-        vm.prank(aliceSwapper);
-        liqRouter.modifyLiquidity{value: uint256(liquidity)}(
-            ethKeyNoHook,
-            IPoolManager.ModifyLiquidityParams({
-                tickLower: tickLower,
-                tickUpper: tickUpper,
-                liquidityDelta: liquidity,
-                salt: 0
-            }),
-            ""
-        );
-        vm.prank(aliceSwapper);
-        liqRouter.modifyLiquidity(
-            keyNoHook2,
-            IPoolManager.ModifyLiquidityParams({
-                tickLower: tickLower,
-                tickUpper: tickUpper,
-                liquidityDelta: liquidity,
-                salt: 0
-            }),
-            ""
-        );
-        vm.prank(aliceSwapper);
-        liqRouter.modifyLiquidity(
-            keyNoHook3,
-            IPoolManager.ModifyLiquidityParams({
-                tickLower: tickLower,
-                tickUpper: tickUpper,
-                liquidityDelta: liquidity,
-                salt: 0
-            }),
-            ""
-        );
-        vm.prank(aliceSwapper);
-        liqRouter.modifyLiquidity(
-            keyNoHook4,
-            IPoolManager.ModifyLiquidityParams({
-                tickLower: tickLower,
-                tickUpper: tickUpper,
-                liquidityDelta: liquidity,
-                salt: 0
-            }),
-            ""
-        );
-        vm.prank(aliceSwapper);
-        liqRouter.modifyLiquidity(
-            keyNoHook5,
-            IPoolManager.ModifyLiquidityParams({
-                tickLower: tickLower,
-                tickUpper: tickUpper,
-                liquidityDelta: liquidity,
-                salt: 0
-            }),
-            ""
-        );
-        /*vm.prank(aliceSwapper);
-        liqRouter.modifyLiquidity(
-            keyNoOpSwapHook,
-            IPoolManager.ModifyLiquidityParams({
-                tickLower: tickLower,
-                tickUpper: tickUpper,
-                liquidityDelta: liquidity,
-                salt: 0
-            }),
-            ""
-        );*/
+        PoolKey[] memory allPoolKeys =
+            _concatPools(vanillaPoolKeys, nativePoolKeys, hookedPoolKeys, csmmPoolKeys);
+        _initializePools(allPoolKeys);
+        _addLiquidity(allPoolKeys, 10_000e18);
     }
 
-    function _sortAddresses(address[] memory addresses) internal pure returns (address[] memory) {
-        for (uint256 i; i < addresses.length; i++) {
-            for (uint256 j = i + 1; j < addresses.length; j++) {
-                if (uint160(addresses[i]) > uint160(addresses[j])) {
-                    address temp = addresses[i];
-                    addresses[i] = addresses[j];
-                    addresses[j] = temp;
-                }
-            }
-        }
-        return addresses;
-    }
+    function test_multi_exactInput() public {}
+    function test_multi_exactInput_native() public {}
+    function test_multi_exactInput_hookData() public {}
+    function test_multi_exactInput_customCurve() public {}
 
-    function test_exactInput_multiSwap() public {
-        // keyNoHook.c0 -> keyNoHook.c1 -> keyNoHook3.c1
-        // currency0 -> currency1 -> currency3
-        PathKey[] memory path = new PathKey[](2);
-        path[0] = PathKey({
-            intermediateCurrency: keyNoHook.currency1,
-            fee: keyNoHook.fee,
-            tickSpacing: keyNoHook.tickSpacing,
-            hooks: keyNoHook.hooks,
-            hookData: ""
-        });
-        path[1] = PathKey({
-            intermediateCurrency: keyNoHook3.currency1,
-            fee: keyNoHook3.fee,
-            tickSpacing: keyNoHook3.tickSpacing,
-            hooks: keyNoHook3.hooks,
-            hookData: ""
-        });
-
-        vm.prank(aliceSwapper);
-        router.swapExactTokensForTokens(
-            0.1 ether, 0, keyNoHook.currency0, path, aliceSwapper, block.timestamp + 1
-        );
-    }
-
-    function test_exactInput_multiSwap_withHookData() public {}
-
-    function test_fuzz_exactInput_multiSwap() public {}
-
-    function test_fuzz_multiSwap(
-        bool exactInput,
-        uint256 amount,
-        Currency startCurrency,
-        PathKey[] memory path,
-        address recipient,
-        uint256 deadline
-    ) public {
-        exactInput
-            ? router.swapExactTokensForTokens(amount, 0, startCurrency, path, recipient, deadline)
-            : router.swapTokensForExactTokens(
-                amount, type(uint256).max, startCurrency, path, recipient, deadline
-            );
-        
-        // assert balances
-    }
+    function test_multi_exactOutput() public {}
+    function test_multi_exactOutput_native() public {}
+    function test_multi_exactOutput_hookData() public {}
+    function test_multi_exactOutput_customCurve() public {}
 }
---
+++ test/utils/SwapRouterFixtures.sol
@@ -0,0 +1,193 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import {GasSnapshot} from "forge-gas-snapshot/GasSnapshot.sol";
+
+import {V4SwapRouter} from "../../src/V4SwapRouter.sol";
+import {IPoolManager, PoolManager} from "@v4/src/PoolManager.sol";
+
+import {PoolKey} from "@v4/src/types/PoolKey.sol";
+import {IHooks} from "@v4/src/interfaces/IHooks.sol";
+import {Currency, CurrencyLibrary} from "@v4/src/types/Currency.sol";
+import {Deployers} from "@v4/test/utils/Deployers.sol";
+import {SafeCast} from "@v4/src/libraries/SafeCast.sol";
+
+import {MockERC20} from "@solady/test/utils/mocks/MockERC20.sol";
+
+import {NoOpSwapHook} from "./mocks/hooks/NoOpSwapHook.sol";
+
+import {PoolModifyLiquidityTest} from "@v4/src/test/PoolModifyLiquidityTest.sol";
+
+import {PathKey} from "../../src/libraries/PathKey.sol";
+
+import {MockCurrencyLibrary} from "./mocks/MockCurrencyLibrary.sol";
+import "@forge/console2.sol";
+
+contract SwapRouterFixtures is Deployers {
+    using SafeCast for uint256;
+
+    Currency currencyA;
+    Currency currencyB;
+    Currency currencyC;
+    Currency currencyD;
+
+    uint24 constant FEE = 3000;
+    int24 constant TICK_SPACING = 60;
+    Currency constant native = CurrencyLibrary.ADDRESS_ZERO;
+
+    /// Lifecycle Functions ///
+
+    function _initializePools(PoolKey[] memory poolKeys) internal {
+        for (uint256 i = 0; i < poolKeys.length; i++) {
+            manager.initialize(poolKeys[i], SQRT_PRICE_1_1);
+        }
+    }
+
+    function _addLiquidity(PoolKey[] memory poolKeys, uint256 liquidity) internal {
+        LIQUIDITY_PARAMS.liquidityDelta = liquidity.toInt256();
+
+        PoolKey memory _poolKey;
+        uint256 msgValue;
+        for (uint256 i = 0; i < poolKeys.length; i++) {
+            _poolKey = poolKeys[i];
+            msgValue = _poolKey.currency0 == native ? 100 ether : 0;
+            modifyLiquidityRouter.modifyLiquidity{value: msgValue}(
+                _poolKey, LIQUIDITY_PARAMS, ZERO_BYTES
+            );
+        }
+    }
+
+    /// Setup Functions ///
+
+    function _createSortedCurrencies()
+        internal
+        returns (Currency _currencyA, Currency _currencyB, Currency _currencyC, Currency _currencyD)
+    {
+        // Namespace and prefix 0x4444 avoid collisions
+        address currencyAA = address((0x000000AA) ^ (0x4444 << 144));
+        address currencyBB = address((0x000000BB) ^ (0x4444 << 144));
+        address currencyCC = address((0x000000CC) ^ (0x4444 << 144));
+        address currencyDD = address((0x000000DD) ^ (0x4444 << 144));
+
+        MockERC20 mockToken = new MockERC20("TEST", "TEST", 18);
+        vm.etch(currencyAA, address(mockToken).code);
+        vm.etch(currencyBB, address(mockToken).code);
+        vm.etch(currencyCC, address(mockToken).code);
+        vm.etch(currencyDD, address(mockToken).code);
+
+        _currencyA = Currency.wrap(currencyAA);
+        _currencyB = Currency.wrap(currencyBB);
+        _currencyC = Currency.wrap(currencyCC);
+        _currencyD = Currency.wrap(currencyDD);
+    }
+
+    function _createPoolKeys(address hook) internal view returns (PoolKey[] memory _poolKeys) {
+        _poolKeys = new PoolKey[](4);
+
+        _poolKeys[0] = PoolKey({
+            currency0: currencyA,
+            currency1: currencyB,
+            fee: FEE,
+            tickSpacing: TICK_SPACING,
+            hooks: IHooks(hook)
+        });
+
+        _poolKeys[1] = PoolKey({
+            currency0: currencyB,
+            currency1: currencyC,
+            fee: FEE,
+            tickSpacing: TICK_SPACING,
+            hooks: IHooks(hook)
+        });
+
+        _poolKeys[2] = PoolKey({
+            currency0: currencyC,
+            currency1: currencyD,
+            fee: FEE,
+            tickSpacing: TICK_SPACING,
+            hooks: IHooks(hook)
+        });
+
+        _poolKeys[3] = PoolKey({
+            currency0: currencyA,
+            currency1: currencyD,
+            fee: FEE,
+            tickSpacing: TICK_SPACING,
+            hooks: IHooks(hook)
+        });
+    }
+
+    function _createNativePoolKeys(address hook)
+        internal
+        view
+        returns (PoolKey[] memory nativePoolKeys)
+    {
+        nativePoolKeys = new PoolKey[](4);
+        nativePoolKeys[0] = PoolKey({
+            currency0: native,
+            currency1: currencyA,
+            fee: FEE,
+            tickSpacing: TICK_SPACING,
+            hooks: IHooks(hook)
+        });
+
+        nativePoolKeys[1] = PoolKey({
+            currency0: native,
+            currency1: currencyB,
+            fee: FEE,
+            tickSpacing: TICK_SPACING,
+            hooks: IHooks(hook)
+        });
+
+        nativePoolKeys[2] = PoolKey({
+            currency0: native,
+            currency1: currencyC,
+            fee: FEE,
+            tickSpacing: TICK_SPACING,
+            hooks: IHooks(hook)
+        });
+
+        nativePoolKeys[3] = PoolKey({
+            currency0: native,
+            currency1: currencyD,
+            fee: FEE,
+            tickSpacing: TICK_SPACING,
+            hooks: IHooks(hook)
+        });
+    }
+
+    /// Utility Functions ///
+
+    function _concatPools(
+        PoolKey[] memory a,
+        PoolKey[] memory b,
+        PoolKey[] memory c,
+        PoolKey[] memory d
+    ) internal pure returns (PoolKey[] memory) {
+        PoolKey[] memory result = new PoolKey[](a.length + b.length + c.length + d.length);
+        uint256 index;
+        for (uint256 i = 0; i < a.length; i++) {
+            result[index] = a[i];
+            index++;
+        }
+        for (uint256 i = 0; i < b.length; i++) {
+            result[index] = b[i];
+            index++;
+        }
+        for (uint256 i = 0; i < c.length; i++) {
+            result[index] = c[i];
+            index++;
+        }
+        for (uint256 i = 0; i < d.length; i++) {
+            result[index] = d[i];
+            index++;
+        }
+        return result;
+    }
+
+    function _copyArrayToStorage(PoolKey[] memory source, PoolKey[] storage destination) internal {
+        for (uint256 i = 0; i < source.length; i++) {
+            destination.push(source[i]);
+        }
+    }
+}

9825503402f4ebdeecdea34d1747e68d7f05f281
ross <92001561+z0r0z@users.noreply.github.com>
2025-01-30 17:00:28 +0000

    ⚡ optimize

--- src/interfaces/IV4SwapRouter.sol
+++ src/interfaces/IV4SwapRouter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.26;
 
 import {PathKey} from "../libraries/PathKey.sol";
 import {PoolKey} from "@v4/src/types/PoolKey.sol";

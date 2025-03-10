7173a5acfdfc46a3945dfc2ede633f6c9fd90826
ross <92001561+z0r0z@users.noreply.github.com>
2025-03-10 14:11:00 +0000

    ⚡ adjust and optimize input amt calc

--- src/base/BaseSwapRouter.sol
+++ src/base/BaseSwapRouter.sol
@@ -86,7 +86,22 @@ abstract contract BaseSwapRouter is SafeCallback {
             (Currency inputCurrency, Currency outputCurrency, BalanceDelta delta, bool zeroForOne) =
                 _parseAndSwap(singleSwap, exactOutput, data.amount, callbackData);
 
-            uint256 inputAmount = uint256(-poolManager.currencyDelta(address(this), inputCurrency));
+            uint256 inputAmount;
+            if (exactOutput) {
+                if (singleSwap) {
+                    // for single-swap exact output, we can use delta
+                    inputAmount = zeroForOne
+                        ? uint256(uint128(-delta.amount0()))
+                        : uint256(uint128(-delta.amount1()));
+                } else {
+                    // for multi-hop exact output, use poolManager.currencyDelta
+                    inputAmount = uint256(-poolManager.currencyDelta(address(this), inputCurrency));
+                }
+            } else {
+                // for exact input (either single or multi), just use data.amount
+                inputAmount = data.amount;
+            }
+
             uint256 outputAmount = exactOutput
                 ? data.amount
                 : (zeroForOne ? uint256(uint128(delta.amount1())) : uint256(uint128(delta.amount0())));

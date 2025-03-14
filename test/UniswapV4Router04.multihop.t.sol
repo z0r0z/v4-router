bc4d6a903540fd28cfd32a0c36a5da901ac76f83
saucepoint <saucepoint@protonmail.com>
2025-03-14 16:41:46 -0400

    test atomic arb with native ether

--- test/UniswapV4Router04.multihop.t.sol
+++ test/UniswapV4Router04.multihop.t.sol
@@ -68,6 +68,10 @@ contract MultihopTest is SwapRouterFixtures {
         PoolKey[] memory _nativePoolKeys = _createNativePoolKeys(address(0));
         _copyArrayToStorage(_nativePoolKeys, nativePoolKeys);
 
+        // create native-eth pools with hookData for arbitrage tests
+        PoolKey[] memory _nativeHookDataPoolKeys = _createNativePoolKeys(address(hookWithData));
+        _copyArrayToStorage(_nativeHookDataPoolKeys, nativePoolKeys);
+
         PoolKey[] memory _hookedPoolKeys = _createPoolKeys(address(hookWithData));
         _copyArrayToStorage(_hookedPoolKeys, hookedPoolKeys);
         PoolKey[] memory _csmmPoolKeys = _createPoolKeys(address(csmm));
@@ -917,7 +921,7 @@ contract MultihopTest is SwapRouterFixtures {
         router.swapExactTokensForTokens(
             10e18,
             9e18,
-            true, // zeroForOne-true, A for B
+            true, // zeroForOne=true, A for B
             PoolKey({
                 currency0: currencyA,
                 currency1: currencyB,
@@ -973,5 +977,73 @@ contract MultihopTest is SwapRouterFixtures {
 
         // verify slippage: recieved > amountOutMin
         assertGt((recipientAfter.currencyA - recipientBefore.currencyA), amountOutMin);
+    }
+
+    /// @dev test atomic arbitrage with native tokens
+    function test_multi_exactInput_arbitrage_nativeInput() public {
+        address recipient = address(0xbeefbeef); // do not fuzz because not all addresses have a receive function
+
+        // arbitrage path: ETH -(vanilla)-> B -(hookData)-> ETH
+        // drive the price of B -(hookData)-> ETH to create arbitrage opportunity
+        // (buy up B on hookData, so it can be sold for more ETH)
+        router.swapExactTokensForTokens{value: 10e18}(
+            10e18,
+            9e18,
+            true, // zeroForOne=true, native for B
+            PoolKey({
+                currency0: native,
+                currency1: currencyB,
+                fee: FEE,
+                tickSpacing: TICK_SPACING,
+                hooks: IHooks(address(hookWithData))
+            }),
+            abi.encode(uint256(1)),
+            address(this),
+            uint256(block.timestamp)
+        );
+
+        TestCurrencyBalances memory thisBefore = currencyBalances(address(this));
+        TestCurrencyBalances memory recipientBefore = currencyBalances(recipient);
+
+        // Swap Path: ETH -(vanilla)-> B -(hookData)-> ETH
+        Currency startCurrency = native;
+        PathKey[] memory path = new PathKey[](2);
+        path[0] = PathKey({
+            intermediateCurrency: currencyB,
+            fee: FEE,
+            tickSpacing: TICK_SPACING,
+            hooks: HOOKLESS,
+            hookData: ZERO_BYTES
+        });
+        path[1] = PathKey({
+            intermediateCurrency: native,
+            fee: FEE,
+            tickSpacing: TICK_SPACING,
+            hooks: IHooks(address(hookWithData)),
+            hookData: abi.encode(uint256(2))
+        });
+
+        uint256 amountIn = 1e18;
+        uint256 amountOutMin = 0.99e18;
+        router.swapExactTokensForTokens{value: amountIn}(
+            amountIn, amountOutMin, startCurrency, path, recipient, uint256(block.timestamp)
+        );
+
+        TestCurrencyBalances memory thisAfter = currencyBalances(address(this));
+        TestCurrencyBalances memory recipientAfter = currencyBalances(recipient);
+
+        // Check balances
+        // test contract did not spend native
+        assertEq(thisBefore.native, thisBefore.native);
+
+        // currencyB unspent
+        assertEq(thisBefore.currencyB, thisAfter.currencyB);
+        assertEq(recipientBefore.currencyB, recipientAfter.currencyB);
+
+        // recipient received native (atomic arbitrage)
+        assertGt(recipientAfter.native, recipientBefore.native);
+
+        // verify slippage: recieved > amountOutMin
+        assertGt((recipientAfter.native - recipientBefore.native), amountOutMin);
     }
 }
